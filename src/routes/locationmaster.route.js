1d5662e806c5ae468e8ae9ebea096889171f3ebc
sansiri25 <aisiri25@gmail.com>
2024-04-27 17:18:19 +0530

    locationroute

--- src/routes/locationmaster.route.js
+++ src/routes/locationmaster.route.js
@@ -10,7 +10,7 @@ router.post('/location/create', locationMasterService.createLocation);
 router.put('/location_details/:id', locationMasterService.updateLocation);
 
 router.get('/location_details', locationMasterService.getLocation); // Get all vendors route
-router.get('/location_details/:id', locationMasterService.getlocationById); // Get vendor by ID route
+router.get('/location/:id', locationMasterService.getlocationById); // Get vendor by ID route
 
 module.exports = router;
 
